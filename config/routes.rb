8182928efb73996691f37fdf1ab72f819eb44fa6
EC2 Default User <ec2-user@ip-172-31-48-218.ec2.internal>
2021-10-15 06:44:03 +0000

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  
+  root to: 'homes#top'
+  get '/home/about', to: 'homes#about', as: 'about'
+  
+  resources :books
+  resources :users, except: [:create]
+  
+end
