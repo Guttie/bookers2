786a5696886238ccd41c37fb45a3824636a0a0fb
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-10-17 08:35:20 +0000

    Fix profile image related

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f38bddd02d709c9c25b54087ed65d3725c386ccfe5fd649538c12dbb4a4d98e5989735158793104ed8aaa1ec11366eeb18bb96e0cbba1dba7472e8de395588f9'
