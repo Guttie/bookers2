38e02243405cf0e32e5dc7b225121411b7f70439
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-11-04 08:00:05 +0000

    [Add]コメントのインスタンス変数

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @newbook = Book.new
+    @book_comment = BookComment.new
   end
 
   def edit
