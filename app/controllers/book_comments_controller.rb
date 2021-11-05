bec522ec1476823ad2a5ff44bb64de9d9ad5c815
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-11-05 06:18:12 +0000

    [Add]createアクション

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,25 @@
 class BookCommentsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:show, :create]
+
   def create
-    
+    @book = Book.find(params[:book_id])
+    book_comment = BookComment.new(book_comment_params)
+    book_comment.user_id = current_user.id
+    if book_comment.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
-  
+
   def destroy
-    
+
   end
-  
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment, :book_id).merge(user_id: current_user.id, book_id: params[:book_id])
+  end
+
 end
