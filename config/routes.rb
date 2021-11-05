16a5e3313796f00cd3e1440509138da1ac94a55c
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-11-05 07:23:02 +0000

    [Add]いいね機能ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
-  
+
   root to: 'homes#top'
   get '/home/about', to: 'homes#about', as: 'about'
-  
+
   resources :books do
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users
-  
+
 end
