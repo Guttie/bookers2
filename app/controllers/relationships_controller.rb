ba94275696a01019ee23b4fcb129d63038a0ab1e
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-11-06 09:55:42 +0000

    [Add]create/destroyアクション

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,19 +1,19 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
   
   #フォローするとき
   def create
+    following = current_user.relationships.build(follower_id: params[:user_id])
+    following.save
+    redirect_to request.referrer
   end
   
   #フォロー外すとき
   def destroy
+    following = current_user.relationships.find_by(follower_id: params[:user_id])
+    following.destroy
+    redirect_to request.referrer
   end
   
-  #フォロー一覧
-  def followings
-  end
-
-  #フォロワー一覧
-  def follower_users
-  end
 
 end
