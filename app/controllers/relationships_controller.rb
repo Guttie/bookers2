02b9f8a94ce95ae6bdb2d3be15f57507d7a30913
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-11-06 08:25:20 +0000

    [Add]アクション

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,18 @@
 class RelationshipsController < ApplicationController
+  
+  #フォローするとき
+  def create
+  end
+  
+  #フォロー外すとき
+  def destroy
+  end
+  
+  #フォロー一覧
   def following_users
   end
 
+  #フォロワー一覧
   def follower_users
   end
 end
