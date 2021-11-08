6081938ce36d6f3c4491a4456329a2d610159d16
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-11-08 10:21:45 +0000

    [Add]検索結果メソッド

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,43 @@
 class SearchesController < ApplicationController
+  
   def search
+    @value = params["search"]["value"]
+    @model = params["search"]["model"]
+    @how = params["search"]["how"]
+    @datas = search_for(@value, @model, @how)
+  end
+  
+  private
+  
+  def complete(value, model)
+    if model == 'User'
+      User.where(name: value)
+    elsif model == 'Book'
+      Book.where(title: value)
+    end
+  end
+  
+  def forward(value, model)
+    if model == 'User'
+      User.where("name LIKE ?", "#{value}%")
+    elsif model == 'Book'
+      Book.where("title LIKE ?", "#{value}%")
+    end
+  end
+  
+  def backward(value, model)
+    if model == 'User'
+      User.where("name LIKE ?", "%#{value}")
+    elsif model == 'Book'
+      Book.where("title LIKE ?", "%#{value}")
+    end
+  end
+  
+  def partical(value, model)
+    if model == 'User'
+      User.where("name LIKE ?", "%#{value}%")
+    elsif model == 'Book'
+      Book.where("title LIKE ?", "%#{value}%")
+    end
   end
 end
