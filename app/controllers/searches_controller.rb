7ca530a32539f88202aff55a11197cde31b30e33
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-11-08 13:26:58 +0000

    [Add]条件分岐メソッド

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,14 +1,14 @@
 class SearchesController < ApplicationController
-  
+
   def search
     @value = params["search"]["value"]
     @model = params["search"]["model"]
     @how = params["search"]["how"]
     @datas = search_for(@value, @model, @how)
   end
-  
+
   private
-  
+
   def complete(value, model)
     if model == 'User'
       User.where(name: value)
@@ -16,7 +16,7 @@ class SearchesController < ApplicationController
       Book.where(title: value)
     end
   end
-  
+
   def forward(value, model)
     if model == 'User'
       User.where("name LIKE ?", "#{value}%")
@@ -24,7 +24,7 @@ class SearchesController < ApplicationController
       Book.where("title LIKE ?", "#{value}%")
     end
   end
-  
+
   def backward(value, model)
     if model == 'User'
       User.where("name LIKE ?", "%#{value}")
@@ -32,12 +32,26 @@ class SearchesController < ApplicationController
       Book.where("title LIKE ?", "%#{value}")
     end
   end
-  
-  def partical(value, model)
+
+  def partial(value, model)
     if model == 'User'
       User.where("name LIKE ?", "%#{value}%")
     elsif model == 'Book'
       Book.where("title LIKE ?", "%#{value}%")
     end
   end
+
+  def search_for(value, model, how)
+    case how
+    when 'complete'
+      complete(value, model)
+    when 'forward'
+      forward(value, model)
+    when 'backward'
+      backward(value, model)
+    when 'partial'
+      partial(value, model)
+    end
+  end
+
 end
